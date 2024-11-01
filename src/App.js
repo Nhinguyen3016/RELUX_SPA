67f604661a9922ffeecb00a70e3315a0c7d96a54
Your New Name <your.email@example.com>
2024-11-01 08:13:03 +0700

    change2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import Home from './components/Home';
-import Section from './components/Section';
-import Services from './components/Services';
-import RelaxSection from './components/RelaxSection';
-import MeetOurTeam from './components/MeetOurTeam';
-import MainLayout from './layout/MainLayout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import HomePage from './page/home/HomePage';
+import GalleryPage from './page/gallery/GalleryPage';
+import BookingPage from './page/booking/BookingPage';
+import TeamOurPage from './page/ourteam/OurTeamPage.jsx';
+import ContactPage from './page/contact/ContactPage';
+import LoginPage from './page/account/login/LoginPage';
+import RegisterPage from './page/account/register/RegisterPage';
+import ForgotPasswordPage from './page/account/login/ForgotPasswordPage';
+import NewPasswordPage from './page/account/login/NewPasswordPage';
+import ServicesPage from './page/services/ServicesPage.jsx';
+import BodyTreatments from './page/services/component/BodyTreatments.jsx';
+import Facials from './page/services/component/Facials.jsx';
+import Massages from './page/services/component/Massages.jsx';
+import SpaPrograms from './page/services/component/SpaPrograms.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <MainLayout>
-        <Home />
-        <Section />
-        <Services />
-        <RelaxSection />
-        <MeetOurTeam />
-      </MainLayout>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/team-our" element={<TeamOurPage />} />
+            <Route path="/account" element={<LoginPage />} />
+            <Route path="/contacts" element={<ContactPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/new-password" element={<NewPasswordPage />} />
+            <Route path="/booking" element={<BookingPage />} />
+            {/* Services routes - separated */}
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/services/body-treatments" element={<BodyTreatments />} />
+            <Route path="/services/facials" element={<Facials />} />
+            <Route path="/services/massages" element={<Massages />} />
+            <Route path="/services/spa-programs" element={<SpaPrograms />} />
+          </Routes>
+        </MainLayout>
       </div>
     </Router>
   );
---
+++ src/page/account/component/FormField.jsx
@@ -0,0 +1,9 @@
+import '../../../styles/account/component/Form.css';
+
+const FormField = ({ children }) => {
+  return (
+    <div className="form-field">{children}</div>
+  )
+}
+
+export default FormField
